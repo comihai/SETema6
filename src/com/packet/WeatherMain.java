e48d673ae934e15fd569cb1b03a587ef808cbdc1
comihai <coca.mihai5@gmail.com>
2014-11-23 23:08:40 +0200

    added link for download json library

--- src/com/packet/WeatherMain.java
+++ src/com/packet/WeatherMain.java
@@ -7,6 +7,10 @@ import com.packet.views.WeatherView;
 /**
  * Created by mihai on 11/21/2014.
  * This is the Main class that represents the entry point to our program
+ *
+ * In this project I used instances of JSONObject class to manage json data returned by the web service.
+ * Download the ZIP file from #http://www.java2s.com/Code/JarDownload/java/java-json.jar.zip and extract it to get the Jar.
+ * Add the Jar to your build path.
  */
 public class WeatherMain {
     public static void main(String[] args) {
